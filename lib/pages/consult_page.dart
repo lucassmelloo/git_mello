f79bd4cb1b4614235827bc527369d80811c8fb00
lucassmelloo <lucasmellovieira99@gmail.com>
2021-05-04 22:28:33 -0300

    Pensando como pegar o usuario da pessoa

--- lib/pages/consult_page.dart
+++ lib/pages/consult_page.dart
@@ -10,6 +10,7 @@ class ConsultPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        /* gitUser : lucassmello; */
         body: Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
