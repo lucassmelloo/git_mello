5db22d09eb69f97fe247559c244987e17e4decf9
lucassmelloo <lucasmellovieira99@gmail.com>
2021-05-08 17:26:43 -0300

    Criando usercard

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:git_mello/core/app_colors.dart';
 import 'package:git_mello/core/app_gradients.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ResultPage extends StatelessWidget {
   const ResultPage({Key key}) : super(key: key);
@@ -13,16 +14,54 @@ class ResultPage extends StatelessWidget {
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(gradient: AppGradients.linear),
         child: SafeArea(
-          child: Column(children: [
-            SizedBox(
-              height: 50,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Container(
+              width: 350,
+              height: 150,
+              decoration: BoxDecoration(
+                  color: AppColors.white,
+                  borderRadius: BorderRadius.circular(75)),
+              child: Row(
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image:
+                            NetworkImage("https://github.com/lucassmelloo.png"),
+                        fit: BoxFit.fill,
+                      ),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Lucas de Mello Vieira",
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        Text(
+                          "lucassmelloo",
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
+            SizedBox(height: 15),
             Container(
               width: 375,
-              height: 700,
+              height: 575,
               decoration: BoxDecoration(
                 color: AppColors.white,
-                borderRadius: BorderRadius.circular(25),
+                borderRadius: BorderRadius.circular(15),
               ),
             ),
           ]),
---
+++ lib/shared/models/owner_model.dart
@@ -0,0 +1,6 @@
+class Owner {
+  String gitUser = "lucassmelloo";
+  String gitName = "Lucas de Mello Vieira";
+  String photoUrl = "https://github.com/lucassmelloo.png";
+  int numbRepos = 11;
+}
