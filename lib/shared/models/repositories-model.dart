4f14137dc1d22fcd9973852074bb12a7f38aa743
lucassmelloo <lucasmellovieira99@gmail.com>
2021-05-28 20:58:33 -0300

    Criando construtor do Repos

--- lib/shared/models/repositories-model.dart
+++ lib/shared/models/repositories-model.dart
@@ -1,5 +1,29 @@
-class Repositories {
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class RepositoryModel {
   String? name;
   String? language;
   int? stargazersCount;
+
+  RepositoryModel({this.name, this.language, this.stargazersCount})
+
+  Future<RepositoryModel> setRepos(user) async {
+    const String apiGithub = "https://api.github.com/users/";
+    final responseRepos = await http.get(Uri.parse(apiGithub + user + "/repos"));
+    final RepositoryModel repos = RepositoryModel.fromJson(responseRepos.body);
+    return repos;
+  }
+
+  factory RepositoryModel.fromMap(Map<String, dynamic> map) {
+    return RepositoryModel(
+      name: map['name'],
+      language: map['language'],
+      stargazersCount: map['stargazers_count'],
+    );
+  }
+
+  factory RepositoryModel.fromJson(String source) =>
+      RepositoryModel.fromMap(json.decode(source));
 }
