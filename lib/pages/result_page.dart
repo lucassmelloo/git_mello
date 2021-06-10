62c9f934bdf5f69117424a4e8c6ed8f5ce885d93
lucassmelloo <lucasmellovieira99@gmail.com>
2021-06-09 22:18:44 -0300

    OVERLAY

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -14,6 +14,7 @@ class ResultPage extends StatefulWidget {
   _ResultPageState createState() => _ResultPageState();
 }
 
+/*Colocar OVERLAy*/
 validationOwner(OwnerModel? owner) {
   if (owner != null) return owner;
 }
